1c276d9b94e59843ff39f137052db8716af34c3d
kosts0 <skld0@bk.ru>
2021-03-31 21:15:33 +0700

    Добавьте файлы проекта.

---
+++ helloapp/Services/SmsMessageSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloapp.Services
+{
+    public class SmsMessageSender
+    {
+        public string Send()
+        {
+            return "Send by SMS";
+        }
+    }
+}
